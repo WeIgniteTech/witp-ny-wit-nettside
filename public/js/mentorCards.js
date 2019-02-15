54058ecb5895f06281a89d2e8def5437267befab
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2019-02-15 23:28:53 +0100

    added mentorCards. When I put it in the nabus site they bloked the text, now I got them behind and will look at it tmrw

---
+++ public/js/mentorCards.js
@@ -0,0 +1,13 @@
+'use strict';     
+$().ready(function() {
+            $('[rel="tooltip"]').tooltip();
+        });
+        function rotateCard(btn) {
+            var $card = $(btn).closest('.card-container');
+            console.log($card);
+            if ($card.hasClass('hover')) {
+                $card.removeClass('hover');
+            } else {
+                $card.addClass('hover');
+            }
+        };
--- public/js/myCustom.js
+++ public/js/myCustom.js
@@ -11,10 +11,10 @@ $(document).ready(function () {
    //     tagName: 'wittest1',
        limit: 20,
         resolution: 'standard_resolution', //could also wrap them in a link
-        accessToken: '', //Todo: instert token
+        accessToken: '257728237.1677ed0.da14365741ae4d87a3154b86dda4e81d', //todo: instert token
         sortBy: 'most-recent', //eller mostl-liked
         //template is where the magic happens
-        template: '<div class="col-lg-3 gallery instafeed"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a> </div>',
+        template: '<div class="col-lg-3 col-sm-3 col-xs-5 instafeed"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a> </div>',
         //to go to instagram when click on picture change href="" to link.
     });
     userfeed.run();
