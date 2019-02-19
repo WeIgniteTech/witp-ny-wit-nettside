ab45b6690dbd913e61e2f383ea0a823eb7092918
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2019-02-19 17:38:52 +0100

    Witp jumbotron edit

--- public/js/aboutUs.js
+++ public/js/aboutUs.js
@@ -28,3 +28,16 @@ $(document).ready(function () {
         }
     });
 });
+'use strict';     
+$().ready(function() {
+            $('[rel="tooltip"]').tooltip();
+        });
+        function rotateCard(btn) {
+            var $card = $(btn).closest('.card-container');
+            console.log($card);
+            if ($card.hasClass('hover')) {
+                $card.removeClass('hover');
+            } else {
+                $card.addClass('hover');
+            }
+        };
