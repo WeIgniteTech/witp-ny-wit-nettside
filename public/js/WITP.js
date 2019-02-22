de0b9a160e8c72a464587c9a685c4d27dbe454d1
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2019-02-22 11:16:15 +0100

    getting navbar to collapse on scroll

--- public/js/WITP.js
+++ public/js/WITP.js
@@ -1,3 +1,32 @@
+//Navbar collapse on scroll
+//function checkOffset() {
+  //  $(".navbar-fixed-top").toggleClass("top-nav-collapse", $(".navbar").offset().top > 50);
+//}
+
+
+$(document).ready(function() {
+
+    // Put your offset checking in a function
+    function checkOffset() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar").addClass("top-nav-collapse");
+        }     
+        else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    }
+
+    //When the page loads
+    checkOffset();
+
+
+    //Run when scrolling
+    $(window).scroll(function() {
+        checkOffset();
+    });
+});
+
+//Form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
