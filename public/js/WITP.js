f73333c2d73c385b577bd53ce623ef74d66fcc11
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2019-02-22 12:13:44 +0100

    show navbar on scroll

--- public/js/WITP.js
+++ public/js/WITP.js
@@ -1,14 +1,4 @@
-/* Hide the navbar when scroll down, show when scroll up. */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-20px";
-    }
-    prevScrollpos = currentScrollPos;
-}
+/* Hide the navbar when scroll down, show when scroll up.  */
 
 //Form
 function openForm() {
